a1afbe5057a28654d6652a1a3e5c1a5b6c40babd
Kushagra Gupta <kushagra357@gmail.com>
2022-05-29 14:39:36 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+// import React,{useState} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// const App = () => {
+//   let [count,setCount]= useState(0)
+//   let [interval,setinterval]= useState(0)
+//   const increment=()=>{
+//     setCount(count=count+1)
+//   }
+//   const decrement=()=>{
+//     setCount(count=count-1)
+//   }
+//   const start=()=>{
+//     let interval = setInterval(increment,1000)
+//     setinterval( interval= interval )
+//   }
+//   const stop = ()=>{
+//     clearInterval(interval)
+//   }
+//   return (
+//     <div>
+//       Count:{count}
+//       <button onClick={increment}>Increment</button>
+//       <button onClick={decrement}>Decrement</button>
+//       <button onClick={start}>Start</button>
+//       <button onClick={stop}>Stop</button>
+//     </div>
+//   )
+// }
+
+// export default App
+
+import React, { Component } from "react";
+
+import Stopwatch from "./Stopwatch";
+import Countdown from "./Countdown";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+        <div className="Timers">
+          <Stopwatch />
+          {/* <Countdown /> */}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
